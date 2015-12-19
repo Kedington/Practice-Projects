f180ee1398d734045f10a96233479ffc549da11c
Kevin <kedingto@ucsd.edu>
2015-12-18 23:58:48 -0800

    I am adding these now to show grant how this works dispite them not really being complete

--- orderArray.java
+++ orderArray.java
@@ -38,7 +38,7 @@ public class orderArray
             for(int i =0; i<sorted.length; i++) System.out.println("" + sorted[i]); //prints sorted array
             }
 
-          public static void betterSort(int[] array) //more optimal way to sort an array.                        
+          public static void betterSort(int[] array) //not actually better doesnt sort every array .                        
             { 
              int current;
            
